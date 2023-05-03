fabfb8d1ffff548152b3deb88d180c2936f9ad15
Aung Pyae Sone <104271849+AungPyaeSone1000@users.noreply.github.com>
2023-05-03 13:20:29 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+let randomNumber1 = RandomNumGenerator(1,6);
+
+let img1 = document.getElementsByClassName("img1")[0];
+img1.src = "images/dice"+randomNumber1+".png";
+
+let randomNumber2 = RandomNumGenerator(1,6);
+
+let img2 = document.getElementsByClassName("img2")[0];
+img2.src = "images/dice"+randomNumber2+".png";
+
+if (randomNumber1 > randomNumber2){
+    document.getElementsByTagName("h1")[0].innerHTML = "Player 1 won!!";
+}
+if (randomNumber2 > randomNumber1){
+    document.getElementsByTagName("h1")[0].innerHTML = "Player 2 won!!";
+}
+if (randomNumber1 == randomNumber2) {
+    document.getElementsByTagName("h1")[0].innerHTML = "It's draw";
+}
+
+
+function RandomNumGenerator(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function refreshPage (){
+    document.body.style.opacity = 0;
+  setTimeout(function() {
+    location.reload();
+  }, 500);
+}
+
+function restart(){
+    document.getElementsByTagName("h1")[0].innerHTML = "Press the Play button";
+    img1.src = "images/dice6.png";
+    img2.src = "images/dice6.png";
+
+}
